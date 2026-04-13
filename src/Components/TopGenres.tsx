81cb162e4bf4a14e2eb326a45b991a1a7e166e63
DanielSteele1 <dsteele1906@gmail.com>
2026-04-13 14:51:56 +0100

    more css changes

--- src/Components/TopGenres.tsx
+++ src/Components/TopGenres.tsx
@@ -1,44 +1,5 @@
-import { RadarChart } from '@mantine/charts';
 
 function TopGenres() {
-
-    const data = [
-
-        {
-            genre: 'Pop',
-            streams: '8',
-        },
-
-        {
-            genre: 'Rock',
-            streams: '10',
-        },
-
-        {
-            genre: 'Rap',
-            streams: '12',
-        },
-
-        {
-            genre: 'EDM',
-            streams: '13',
-        },
-
-        {
-            genre: 'Jazz',
-            streams: '8',
-        },
-
-        {
-            genre: 'Lo-Fi',
-            streams: '9',
-        },
-
-        {
-            genre: 'Classical',
-            streams: '2',
-        },
-    ]
 
     return (
         <section className="Posts-widget">
